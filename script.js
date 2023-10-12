fc587270da8f56f645619a537d4c34622eddeefd
Ochieng John Ronnie <johnronnie18@gmail.com>
2023-10-12 18:01:26 +0300

    successfully fetching of data and searching for artist by name

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-const userTemplate = document.getElementById('user-template')
-const usercard = document.querySelector("[usercard]")
-const searchInput = document.getElementById('search')
+const userTemplate = document.querySelector(".user-template");
+const usercard = document.querySelector(".usercard"); 
+const searchInput = document.querySelector(".search"); 
+let users =[]
 
-searchInput.addEventListener('search', (e)=>{
-    const value = e.target.value
-    console.log(value)
-})
 
+
+searchInput.addEventListener('input', (e) => {
+    const value = e.target.value.toLowerCase();
+    users.forEach(user => {
+        const isVisible = (typeof user.title === 'string' && user.title.toLowerCase().includes(value)) || 
+                          (typeof user.artist === 'string' && user.artist.toLowerCase().includes(value));
+        user.element.classList.toggle("hide", !isVisible);
+    });
+});
 
 fetch("db.json")
-.then((res)=> res.json())
-.then((data) =>{
-    data.forEach(user =>{  
-    const card = userTemplate.content.cloneNode(true).children[0]
-    const songName = card.querySelector("[song-name]")
-    const artistName = card.querySelector("[artist-name]")
-    const artistCover = card.querySelector("[artist-cover]")
-    songName.textContent = user.title
-    artistName.textContent = user.artist
-    artistCover.src = user.artwork
-    usercard.append(card)
-
-    })
-    
-})
+    .then((res) => res.json())
+    .then((data) => {
+        users = data.songs.map(user => {
+            const card = userTemplate.content.cloneNode(true).children[0];
+            const songName = card.querySelector("[song-name]");
+            const artistName = card.querySelector("[artist-name]");
+            const artistCover = card.querySelector(".image");
+            songName.textContent = user.title;
+            artistName.textContent = user.artist;
+            artistCover.src = user.artwork;
+            usercard.append(card);
+            return {  Song: user.title, artist: user.artist, element: card };
+        });
+    });
